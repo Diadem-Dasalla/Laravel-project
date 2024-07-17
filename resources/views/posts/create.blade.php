df77ebf1e48ccd96e22caf2666983fe5e1cef1d4
Diadem Mae Dasalla <78059729+Diadem-Dasalla@users.noreply.github.com>
2024-07-17 19:07:13 +1000

    Update create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,1 +1,17 @@
+@extends('layouts.app')
 
+@section('title', 'Create Post')
+
+@section('content')
+    <h1>Create New Post</h1>
+    <form action="{{ route('posts.store') }}" method="POST">
+        @csrf
+        <label for="title">Title</label>
+        <input type="text" name="title" required>
+        
+        <label for="content">Content</label>
+        <textarea name="content" required></textarea>
+        
+        <button type="submit">Create</button>
+    </form>
+@endsection
