3a33f79cae2bf3f92b31fc3ce9c607222f26824f
Diadem Mae Dasalla <78059729+Diadem-Dasalla@users.noreply.github.com>
2024-07-17 19:07:37 +1000

    Update show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,1 +1,15 @@
+@extends('layouts.app')
 
+@section('title', $post->title)
+
+@section('content')
+    <h1>{{ $post->title }}</h1>
+    <p>{{ $post->content }}</p>
+    <a href="{{ route('posts.edit', $post) }}">Edit</a>
+    <form action="{{ route('posts.destroy', $post) }}" method="POST" style="display:inline;">
+        @csrf
+        @method('DELETE')
+        <button type="submit">Delete</button>
+    </form>
+    <a href="{{ route('posts.index') }}">Back to Posts</a>
+@endsection
