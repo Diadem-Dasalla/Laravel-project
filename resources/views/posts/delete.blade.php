b4017ff4133e8f9f994d83d820449b54186c0225
Diadem Mae Dasalla <78059729+Diadem-Dasalla@users.noreply.github.com>
2024-07-17 19:08:29 +1000

    Update delete.blade.php

--- resources/views/posts/delete.blade.php
+++ resources/views/posts/delete.blade.php
@@ -1,1 +1,13 @@
+@extends('layouts.app')
 
+@section('title', 'Delete Post')
+
+@section('content')
+    <h1>Are you sure you want to delete "{{ $post->title }}"?</h1>
+    <form action="{{ route('posts.destroy', $post) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button type="submit">Yes, Delete</button>
+    </form>
+    <a href="{{ route('posts.index') }}">Cancel</a>
+@endsection
