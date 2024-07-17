434fc247e901a24bf9f9d35e32061a847bdf355d
Diadem Mae Dasalla <78059729+Diadem-Dasalla@users.noreply.github.com>
2024-07-17 19:06:42 +1000

    Update index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,1 +1,15 @@
+@extends('layouts.app')
 
+@section('title', 'Posts')
+
+@section('content')
+    <h1>Posts</h1>
+    <!-- Loop through posts and display them -->
+    @foreach ($posts as $post)
+        <div>
+            <h2><a href="{{ route('posts.show', $post) }}">{{ $post->title }}</a></h2>
+            <p>{{ $post->content }}</p>
+        </div>
+    @endforeach
+    <a href="{{ route('posts.create') }}">Create New Post</a>
+@endsection
