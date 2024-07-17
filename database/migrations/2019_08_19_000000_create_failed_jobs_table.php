0e638628113b721981dfd8ecd3650b56fee8c757
Diadem Mae Dasalla <78059729+Diadem-Dasalla@users.noreply.github.com>
2024-07-17 19:23:15 +1000

    Update 2019_08_19_000000_create_failed_jobs_table.php

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -28,6 +28,6 @@ class CreateFailedJobsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('posts');
     }
 }
