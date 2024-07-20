2d86b148cfc4ce896a051b692f4c0ae8e5856c90
Diadem Mae Dasalla <78059729+Diadem-Dasalla@users.noreply.github.com>
2024-07-20 18:22:04 +1000

    Update 2024_07_17_075939_create_posts_table.php

--- database/migrations/2024_07_17_075939_create_posts_table.php
+++ database/migrations/2024_07_17_075939_create_posts_table.php
@@ -14,9 +14,11 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        $table->id();
+        $table->string('title');
+        $table->text('body');
+        $table->timestamps();
+    });
     }
 
     /**
