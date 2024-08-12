5657a261b19c466d8d56ff393f9d8f9472f1c8c0
Diadem Mae Dasalla <dasalladem@gmail.com>
2024-08-12 15:25:00 +1000

    Implemented admin user controller

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::resource('posts', PostController::class);
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
