f828c2e673bf1e1cbde1cc72de8a72566d13c805
Diadem Mae Dasalla <78059729+Diadem-Dasalla@users.noreply.github.com>
2024-08-12 15:37:46 +1000

    Update create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,17 +1,32 @@
-@extends('layouts.app')
-
-@section('title', 'Create Post')
+@extends('layouts.admin')
 
 @section('content')
-    <h1>Create New Post</h1>
-    <form action="{{ route('posts.store') }}" method="POST">
+<div class="container">
+    <h1>Create User</h1>
+    
+    <form action="{{ route('admin.users.store') }}" method="POST">
         @csrf
-        <label for="title">Title</label>
-        <input type="text" name="title" required>
-        
-        <label for="content">Content</label>
-        <textarea name="content" required></textarea>
-        
-        <button type="submit">Create</button>
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="name" name="name" required>
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" name="email" required>
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" name="password" required>
+        </div>
+        <div class="form-group">
+            <label for="role">Role</label>
+            <select class="form-control" id="role" name="role" required>
+                <option value="admin">Admin</option>
+                <option value="author">Author</option>
+                <option value="user">User</option>
+            </select>
+        </div>
+        <button type="submit" class="btn btn-primary">Create</button>
     </form>
+</div>
 @endsection
