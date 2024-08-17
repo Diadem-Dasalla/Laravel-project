5b42d57806b7626161385a05aa02bafbb0627fcf
Diadem Mae Dasalla <78059729+Diadem-Dasalla@users.noreply.github.com>
2024-08-18 02:47:58 +1000

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,1 +1,13 @@
+<!DOCTYPE html>
+
+<div class="form-group">
+    <label for="role">Register as:</label>
+    <select name="role" id="role" class="form-control">
+        <option value="user">User</option>
+        <option value="author">Author</option>
+        @if (auth()->check() && auth()->user()->role === 'admin')
+            <option value="admin">Admin</option>
+        @endif
+    </select>
+</div>
 
