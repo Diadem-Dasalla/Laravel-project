6d5934eae4478cd705578e5eac0bab91eae80cc2
Diadem Mae Dasalla <78059729+Diadem-Dasalla@users.noreply.github.com>
2024-08-18 03:23:46 +1000

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'role' => $data['role'] ?? 'user', // Assign the role, defaulting to 'user' if none is provided
+            'role' => $data['role'], // Assign the role, defaulting to 'user' if none is provided
         ]);
     }
 }
