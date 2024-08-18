2619c6f79979e38e0bc549bd5ffd5d5638d5be38
Diadem Mae Dasalla <78059729+Diadem-Dasalla@users.noreply.github.com>
2024-08-18 14:49:59 +1000

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,10 +18,13 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+    'name', 'email', 'password', 'role_id',
     ];
+
+public function role()
+{
+    return $this->belongsTo(Role::class);
+}
 
     /**
      * The attributes that should be hidden for serialization.
