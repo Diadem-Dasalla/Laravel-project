5c63ec5fb7460ad76b9e7dbd9e03edacbde21e3a
Diadem Mae Dasalla <78059729+Diadem-Dasalla@users.noreply.github.com>
2024-08-18 14:42:16 +1000

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,6 +36,7 @@ class User extends Authenticatable
     protected $attributes = [
         'role' => 'user',
     ];
+    
 
 
     /**
@@ -46,4 +47,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
 }
